2fce689731fce3f4f87e85d9eed85af9a97e0635
ibrahimbakkar <ebraheembakkar9@gmail.com>
2026-07-13 10:46:38 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ibrahim.smartplanner',
+  appName: 'المنظم الزمني الذكي',
+  webDir: 'www'
+};
+
+export default config;
